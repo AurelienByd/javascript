62b2bd0c3892269bfff252af9ec36d4c96716319
aurelien <rubis91999@hotmail.com>
2023-05-09 16:13:16 +0200

    javascript phase 06

--- phase_06/javascript.js
+++ phase_06/javascript.js
@@ -2,5 +2,9 @@ var a = "100";
 var b = 100;
 var c = 1.00;
 var d = true;
+var bbis = --b;
 
-window.alert("Ceci est une chaîne de caractères :" + " " + a )
+window.alert("Ceci est une chaîne de caractères :" + " " + a );
+window.alert("Décrémentation :" + " " + bbis);
+window.alert(c + a)
+window.alert(!d)
