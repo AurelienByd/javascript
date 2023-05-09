98fb588d7486d58d4d419b0942c577774d1bc5d8
aurelien <rubis91999@hotmail.com>
2023-05-09 17:16:12 +0200

    javascript

--- phase_06/javascript.js
+++ phase_06/javascript.js
@@ -6,5 +6,5 @@ var bbis = --b;
 
 window.alert("Ceci est une chaîne de caractères :" + " " + a );
 window.alert("Décrémentation :" + " " + bbis);
-window.alert(c + a)
-window.alert(!d)
+window.alert(c + a);
+window.alert(!d);
---
+++ phase_07/javascript.js
@@ -0,0 +1,10 @@
+var nombre = window.prompt("Ecrivez un nombre :");
+
+if (nombre%2)
+{
+    console.log("nombre pair");
+}
+else
+{
+    console.log("nombre impair");
+}
