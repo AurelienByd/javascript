5606d9edb3edc1b0cbea98695b65efa9832abfed
aurelien <rubis91999@hotmail.com>
2023-05-09 15:31:54 +0200

    javascript phase 05

--- phase_05/javascript.js
+++ phase_05/javascript.js
@@ -1,3 +1,10 @@
 var reponse1 = window.prompt("Votre nom :");
 var reponse2 = window.prompt("Votre prénom :");
 window.alert(reponse1 + " " + reponse2);
+
+var reponse3 = window.prompt("Ecrire un nombre :");
+var reponse4 = window.prompt("Ecrire un nombre :");
+window.alert(reponse3*reponse4);
+
+var reponse5 = window.prompt("Quelle est la température en Celcius ?");
+alert("Voici la température en Fahrenheit :" + " " + ((reponse5*9/5)+32));
