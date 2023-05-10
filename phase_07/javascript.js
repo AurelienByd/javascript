2c4157f0afa3c44b39cc048164c888fb4a266777
aurelien <rubis91999@hotmail.com>
2023-05-10 15:51:47 +0200

    javascript phase 07

--- phase_07/javascript.js
+++ phase_07/javascript.js
@@ -7,3 +7,42 @@ else
 {
     window.alert("nombre impair");
 }
+
+var année = window.prompt("Quelle année sommes-nous ?");
+var age = window.prompt("Quelle est votre année de naissance ?");
+if  ((année-age)>=18)
+{
+    window.alert((année-age) + " " + "an(s)" + "," + " " + "majeur");
+}
+else
+{
+    window.alert((année-age) + " " + "an(s)" + "," + " " + "mineur");
+}
+
+var n1 = parseInt(window.prompt("Saisir un nombre :"));
+var n2 = parseInt(window.prompt("Saisir un nombre :"));
+let o = window.prompt("Saisir un opérateur :");
+if ((o!=="+") && (o!=="-") && (o!=="*") && (o!=="/"))
+{
+    window.alert("ERREUR");
+}
+if (o=="+")
+{
+    window.alert(n1 + n2);
+}
+else if (o=="-")
+{
+    window.alert(n1-n2);
+}
+else if (o=="*")
+{
+    window.alert(n1*n2);
+}
+else if ((o=="/") && (n2!==0))
+{
+    window.alert(n1/n2);
+}
+else if ((o=="/") && (n2==0))
+{
+    window.alert("DIVISION PAR 0 IMPOSSIBLE");
+}
