95d668e2e2aba15805ddae875b719d6e76d85b1b
aurelien <rubis91999@hotmail.com>
2023-05-10 17:50:48 +0200

    phase 08

---
+++ phase_08/javascript.js
@@ -0,0 +1,18 @@
+//Exercice 2
+
+//const n = 30;
+//let m;
+//for (m=0; m<n; ++m)
+//{
+//    console.log(m);
+//}
+
+//Exercice 3
+
+let e1 = parseInt(window.prompt("Ecrire un nombre entier :"));
+let e2 = parseInt(window.prompt("Ecrire un nombre entier :"));
+let e3 = parseInt(window.prompt("Ecrire un nombre entier :"));
+while ((e1 + e2 + e3)/=0)
+{
+    console.log(e1 + e2 + e3);
+}
