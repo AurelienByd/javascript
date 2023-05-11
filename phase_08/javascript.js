66ffc5d32dcd15c7d23371976e520287bf005b49
aurelien <rubis91999@hotmail.com>
2023-05-11 10:09:04 +0200

    javascript phase 08

--- phase_08/javascript.js
+++ phase_08/javascript.js
@@ -1,18 +1,21 @@
 //Exercice 2
 
-//const n = 30;
-//let m;
-//for (m=0; m<n; ++m)
-//{
-//    console.log(m);
-//}
+const n = 30;
+let m;
+for (m=0; m<n; ++m)
+{
+    console.log(m);
+}
 
 //Exercice 3
 
 let e1 = parseInt(window.prompt("Ecrire un nombre entier :"));
 let e2 = parseInt(window.prompt("Ecrire un nombre entier :"));
-let e3 = parseInt(window.prompt("Ecrire un nombre entier :"));
-while ((e1 + e2 + e3)/=0)
+let r1;
+let r2;
+while (r1!==0)
 {
-    console.log(e1 + e2 + e3);
+    r1 = e1 + e2;
+    r2 = (e1 + e2) / 2;
+    window.alert(r1 + " " + "-" + " " + r2);
 }
