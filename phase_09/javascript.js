308c20579b599d1d363a20ca16272fee2ee20868
aurelien <rubis91999@hotmail.com>
2023-05-11 16:59:06 +0200

    javascript

---
+++ phase_09/javascript.js
@@ -0,0 +1,22 @@
+var x = window.prompt("entrez un nombre :");
+var y = window.prompt("entrez un multiplicateur :");
+var v = x*x*x;
+var w = x*y;
+document.getElementById("i").innerHTML = "Le cube de " + x + " est égal à " + v;
+document.getElementById("j").innerHTML = "Le produit de " + x + " x " + y + " est égal à " + w;
+
+function produit(x, y)
+{
+    return v, w;
+}
+
+var imagejavascript = document.createElement("/home/stagiaire/javascript/phase_09/papillon.jpg");
+
+function afficheImg(imagejavascript)
+{
+    var monimage = imagejavascript;
+monimage = "/home/stagiaire/javascript/phase_09/papillon.jpg";
+return monimage;
+}
+
+afficheImg("/home/stagiaire/javascript/phase_09/papillon.jpg");
