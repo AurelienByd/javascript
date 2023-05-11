196e04e3281e67fe40af4b7db8eb2aa46f729717
aurelien <rubis91999@hotmail.com>
2023-05-11 15:46:29 +0200

    javascript

---
+++ phase_10/javascript.js
@@ -0,0 +1,8 @@
+var n = 0;
+var mytableau = Array(parseInt(t = window.prompt("Saisissez la taille du tableau :")));
+while (n<t)
+{
+mytableau[n]=[(window.prompt("Entrez une lettre :"))];
+n++;
+}
+console.table(mytableau);
