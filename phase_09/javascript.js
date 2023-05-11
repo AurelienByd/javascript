eb93a6144241755bcb2f00a7c620de42ac7db8ad
aurelien <rubis91999@hotmail.com>
2023-05-11 15:54:42 +0200

    javascript

--- phase_09/javascript.js
+++
@@ -1,9 +0,0 @@
-function produit(x, y)
-{
-    alert(x);
-    alert(y);
-    x = window.prompt("entrez un nombre :");
-    y = window.prompt("entrez un multiplicateur :");
-    return x*y;
-}
-afficheImg(/home/stagiaire/javascript/phase_09/papillon.jpg)
